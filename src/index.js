ed8638b74e8f3e4601e34bc9bada20c33b051387
Diegoipaez <diegokpo1313@gmail.com>
2023-01-30 10:50:48 -0300

    The way in which the project is rendered has been improved

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import App from './App';
 import './scss/index.scss';
 import 'react-toastify/dist/ReactToastify.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
