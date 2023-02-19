53cb83e3f9249072bdb2538b72da087a9d5fc0a6
Diegoipaez <diegokpo1313@gmail.com>
2023-02-19 17:40:06 -0300

    home page and bannerHome component added

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import "./home.scss";
+import BannerHome from "../../components/BannerHome";
+
+export default function Home() {
+  return (
+    <>
+      <BannerHome />
+      <div className="home">
+        <h2>Mas...</h2>
+      </div>
+    </>
+  );
+}
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,10 +1,11 @@
 import { Routes as Switch, Route } from "react-router-dom";
+import Home from "../pages/Home";
 import Settings from "../pages/Settings/Settings";
 
 export default function Routes({ user }) {
   return (
     <Switch>
-      <Route path="/" element={<h2>Top bar</h2>} />
+      <Route path="/" element={<Home/>} />
       <Route path="/artists" element={<h2>Artists</h2>} />
       <Route path="/settings" element={<Settings user={user} />} />
     </Switch>
