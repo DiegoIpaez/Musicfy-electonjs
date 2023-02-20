8ee8bd1bb844da65d754f8cfedcd3cb0cceb49bd
Diegoipaez <diegokpo1313@gmail.com>
2023-02-20 01:23:19 -0300

    artists page completed

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,12 +2,13 @@ import { Routes as Switch, Route } from "react-router-dom";
 import Artist from "../pages/Artist";
 import Home from "../pages/Home";
 import Settings from "../pages/Settings/Settings";
+import Artists from '../pages/Artists'
 
 export default function Routes({ user }) {
   return (
     <Switch>
       <Route path="/" element={<Home/>} />
-      <Route path="/artists" element={<h2>Artists</h2>} />
+      <Route path="/artists" element={<Artists/>} />
       <Route path="/artist/:id" element={<Artist/>} />
       <Route path="/settings" element={<Settings user={user} />} />
     </Switch>
