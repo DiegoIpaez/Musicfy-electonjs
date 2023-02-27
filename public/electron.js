26f1f17707b0dd1e02f5d45bec1ba90d8ea4e2fd
Diegoipaez <diegokpo1313@gmail.com>
2023-02-26 22:16:36 -0300

    reconfiguring electron

--- public/electron.js
+++ public/electron.js
@@ -1,25 +1,40 @@
-const electron = require('electron');
+const electron = require("electron");
 const BrowserWindow = electron.BrowserWindow;
 
 const app = electron.app;
-const path = require('path');
-const isDev = require('electron-is-dev');
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({
+    width: 1500,
+    height: 1000,
+    resizable: true,
+    kiosk: false,
+    title: 'Musicfy',
+    titleBarStyle: "hiddenInset",
+  });
   mainWindow.loadURL(
-    isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
   );
-  if (isDev) { mainWindow.webContents.openDevTools(); }
-  mainWindow.on('closed', () => mainWindow = null);
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+  mainWindow.on("closed", () => (mainWindow = null));
 }
 
-app.on('ready', createWindow);
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') { app.quit(); }
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
-app.on('activate', () => {
-  if (mainWindow === null) { createWindow(); }
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
