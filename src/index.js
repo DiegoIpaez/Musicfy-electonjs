b0ed1e5584c0e5716732649643c16a1bdd2abb6d
Diegoipaez <diegokpo1313@gmail.com>
2023-02-27 00:29:11 -0300

    added css modules of semantic.ui

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import './scss/index.scss';
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
